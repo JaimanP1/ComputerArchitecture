39dbc8ab375269898c80e98615b939fd66db384e
Jaiman, WSL <jaimanparekh0@gmail.com>
2024-09-16 11:18:06 -0400

    more practice on pointers

--- TextbookPractice/Chap2/ex1.c
+++ TextbookPractice/Chap2/ex1.c
@@ -7,33 +7,44 @@
 
 void bytes(void *start, int length){
 	//given an array (void *) and then print out all elements, which are bytes
-	printf("address, value \n");
+	printf("\naddress, value \n");
 	for(int i=0; i<length; i++){
 		//address
-		printf("address byte %d: %x \n", i+1, *(start+i));
+		printf("address byte %d: %p \n", i+1, (start+i));
 		//value
-		printf("value byte %d: %x \n", i+1, *(start+i));
+		printf("value byte %d: %x \n", i+1, *(char *)(start+i));
 	}
 	printf("\n");
 }
 
 int main(){
-	int integer = 0;
-	char character = "c";
+	int integer = 987654321;
+	char character = 'c';
 	char *string = "The C Programming Language";
 	int *pointer = &integer;
+	int array[] = {1, 2, 3};
 	
-	printf("integer: \n");
+	printf("integer: %d (%x) \n", integer, integer);
 	bytes(&integer, sizeof(int));
-	printf("character: \n");
+
+	printf("character: %c (%x) \n", character, character);
 	bytes(&character, sizeof(char));
-	printf("character array: \n");
-	bytes(void* (string), sizeof(char) * sizeof(string));
-	printf("character array v2: \n");
-	bytes(&string, sizeof(char) * sizeof(string));
-	printf("integer pointer : \n");
-	bytes(void* (pointer), sizeof(int));
-	printf("integer pointer v2: \n");
-	bytes(&pointer, sizeof(int));
+
+	printf("character array: %c (%x) \n", *string, *string); 
+	bytes((void*) (string), sizeof(string)/sizeof(char));
 	
+	printf("character array v2: %s (%p) \n", string, string);
+	bytes(&string, sizeof(char*));
+	
+	printf("integer pointer: %d (%x) \n", *pointer, *pointer);
+	bytes((void*) (pointer), sizeof(int));
+	
+	printf("integer pointer v2: %ls (%p) \n", pointer, pointer);
+	bytes(&pointer, sizeof(int *));
+	
+	printf("array: %d (%x) \n", *array, *array);
+	bytes((void*)array, sizeof(array));
+
+	printf("array v2: %ls (%p) \n", array, array);
+	bytes(&array, sizeof(array));
 }
